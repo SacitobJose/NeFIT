09b95c247e399291f1ea2150f00547c6c44d18ed
JRI98 <38755101+JRI98@users.noreply.github.com>
2020-01-16 21:04:56 +0000

    Code for the catalog (not working)

---
+++ catalog/CompanyApplication.java
@@ -0,0 +1,30 @@
+package catalog;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+import catalog.health.TemplateHealthCheck;
+
+public class CompanyApplication extends Application<CompanyConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new CompanyApplication().run(args);
+    }
+
+    @Override
+    public String getName() { return "Company"; }
+
+    @Override
+    public void initialize(Bootstrap<CompanyConfiguration> bootstrap) { }
+
+    @Override
+    public void run(CompanyConfiguration configuration,
+                    Environment environment) {
+        environment.jersey().register(
+            new CompanyResource(configuration.template, configuration.defaultName));
+        environment.healthChecks().register("template",
+            new TemplateHealthCheck(configuration.template));
+    }
+
+}
+
---
+++ catalog/representations/Saying.java
@@ -0,0 +1,10 @@
+package catalog.representations;
+
+//import com.fasterxml.jackson.annotation.*;
+
+public class Saying {
+    public long id;
+    public String name;
+    public String address;
+}
+
